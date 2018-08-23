c331d7c27d6835bf6881173813958e00a18605b6
Ian Myers <ianmyers12@gmail.com>
2018-08-23 11:16:23 -0400

    Initial

---
+++ NewsLetterAppMVC/Models/NewsLetterContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace NewsLetterAppMVC.Models
+{
+    public partial class NewsLetterContext : DbContext
+    {
+        public NewsLetterContext()
+        {
+        }
+
+        public NewsLetterContext(DbContextOptions<NewsLetterContext> options)
+            : base(options)
+        {
+        }
+
+        // Unable to generate entity type for table 'dbo.SignUps'. Please see the warning messages.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=NewsLetter;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
